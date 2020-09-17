2a53873a4d987f7defac8749202612a4c486b829
Thomas Garayua <tgarayua@gmail.com>
2020-09-17 08:21:40 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,5 +1,5 @@
 def update_status
-  epic_tragedy = {
+epic_tragedy = {
   :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
